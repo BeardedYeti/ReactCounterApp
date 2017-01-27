9bbced6b5ff022ee12c1366a374cddfb4fd058bc
Colin Smith <cmsgoboston41@gmail.com>
2017-01-27 18:49:33 -0500

    Add percentage methods

--- src/components/GameDayCount.js
+++ src/components/GameDayCount.js
@@ -2,23 +2,33 @@ import React from 'react'
 import '../stylesheets/ui.scss'
 
 export const GameDayCount = React.createClass({
+	
+	percentDecimal(decimal) {
+		return ((decimal * 100) + '%')
+	},
+	calcProgress(total, goal) {
+		return this.percentDecimal(total/goal)
+	},
+
 	render() {
 		return (
 			<div className="game-day-count">
 				<div className="total-days">
 					<span>{this.props.total}</span>
-					<span>days</span>
+					<span> days</span>
 				</div>
 				<div className="coop-days">
 					<span>{this.props.coop}</span>
-					<span>days</span>
+					<span> days</span>
 				</div>
 				<div className="livestream-days">
 					<span>{this.props.livestream}</span>
-					<span>days</span>
+					<span> days</span>
 				</div>
 				<div>
-					<span>{this.props.goal}</span>
+					<span>
+						{this.calcProgress(this.props.total, this.props.goal)}
+					</span>
 				</div>
 			</div>
 		)
