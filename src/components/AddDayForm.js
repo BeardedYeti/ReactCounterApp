6c1bb7e0ff16fdb8328ffa60bbba407ed5514ff9
Colin Smith <cmsgoboston41@gmail.com>
2017-02-02 18:40:33 -0500

    Add default values to form

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -2,23 +2,43 @@ import { PropTypes, Component } from 'react'
 
 export class AddDayForm extends Component {
 	render() {
+
+		const { game, date, coop, livestream } = this.props
+
 		return (
 			<form className="add-day-form">
 				<label htmlFor="game">Game:</label>
-				<input id="game" type="text" required />
+				<input id="game" 
+					   type="text" 
+					   required 
+					   defaultValue={game}/>
 				<label htmlFor="date">Date:</label>
-				<input id="date" type="date" required />
+				<input id="date" 
+				       type="date" 
+				       required 
+				       defaultValue={date}/>
 				<div>
-					<input id="coop" type="checkbox" />
+					<input id="coop" 
+						   type="checkbox" 
+						   defaultChecked={coop}/>
 					<label htmlFor="coop">Coop:</label>
 				</div>
 				<div>
-					<input id="livestream" type="checkbox" />
+					<input id="livestream" 
+					       type="checkbox" 
+					       defaultChecked={livestream}/>
 					<label htmlFor="livestream">Livestreamed:</label>
 				</div>
 			</form>
 		)
 	}
+}
+
+AddDayForm.defaultProps = {
+	game: "Overwatch",
+	date: "2017-01-1",
+	coop: true,
+	livestream: false
 }
 
 AddDayForm.propTypes = {
