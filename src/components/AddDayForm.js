aa3286b94f176d3088b902da7caed29bed94edc4
Colin Smith <cmsgoboston41@gmail.com>
2017-02-02 18:54:12 -0500

    Add functional submit button

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,34 +1,53 @@
 import { PropTypes, Component } from 'react'
 
 export class AddDayForm extends Component {
+
+	constructor(props) {
+		super(props)
+		this.submit = this.submit.bind(this)
+	}
+	
+	submit(e) {
+		e.preventDefault()
+		console.log('game', this.refs.game.value)
+		console.log('date', this.refs.date.value)
+		console.log('coop', this.refs.coop.checked)
+		console.log('livestream', this.refs.livestream.checked)
+	}
+
 	render() {
 
 		const { game, date, coop, livestream } = this.props
 
 		return (
-			<form className="add-day-form">
+			<form onSubmit={this.submit} className="add-day-form">
 				<label htmlFor="game">Game:</label>
 				<input id="game" 
 					   type="text" 
 					   required 
-					   defaultValue={game}/>
+					   defaultValue={game}
+					   ref="game"/>
 				<label htmlFor="date">Date:</label>
 				<input id="date" 
 				       type="date" 
 				       required 
-				       defaultValue={date}/>
+				       defaultValue={date}
+				       ref="date"/>
 				<div>
 					<input id="coop" 
 						   type="checkbox" 
-						   defaultChecked={coop}/>
+						   defaultChecked={coop}
+						   ref="coop"/>
 					<label htmlFor="coop">Coop:</label>
 				</div>
 				<div>
 					<input id="livestream" 
 					       type="checkbox" 
-					       defaultChecked={livestream}/>
+					       defaultChecked={livestream}
+					       ref="livestream"/>
 					<label htmlFor="livestream">Livestreamed:</label>
 				</div>
+				<button>Add</button>
 			</form>
 		)
 	}
