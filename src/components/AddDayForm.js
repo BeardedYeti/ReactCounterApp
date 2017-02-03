ec16862d44f1210c017cbe5ef447d297ee791758
Colin Smith <cmsgoboston41@gmail.com>
2017-02-03 07:49:30 -0500

    Refactor AddDayForm to be stateless

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,56 +1,49 @@
 import { PropTypes, Component } from 'react'
 
-export class AddDayForm extends Component {
-
-	constructor(props) {
-		super(props)
-		this.submit = this.submit.bind(this)
-	}
+export const AddDayForm = ({ game, date, coop, livestream }) => {
 	
-	submit(e) {
+	const submit = (e) => {
 		e.preventDefault()
-		console.log('game', this.refs.game.value)
-		console.log('date', this.refs.date.value)
-		console.log('coop', this.refs.coop.checked)
-		console.log('livestream', this.refs.livestream.checked)
+		console.log('game', _game.value)
+		console.log('date', _date.value)
+		console.log('coop', _coop.checked)
+		console.log('livestream', _livestream.checked)
 	}
 
-	render() {
+	let _game, _date, _coop, _livestream
 
-		const { game, date, coop, livestream } = this.props
+	return (
+		<form onSubmit={submit} className="add-day-form">
+			<label htmlFor="game">Game:</label>
+			<input id="game" 
+				   type="text" 
+				   required 
+				   defaultValue={game}
+				   ref={input => _game = input}/>
+			<label htmlFor="date">Date:</label>
+			<input id="date" 
+			       type="date" 
+			       required 
+			       defaultValue={date}
+			       ref={input => _date = input}/>
+			<div>
+				<input id="coop" 
+					   type="checkbox" 
+					   defaultChecked={coop}
+					   ref={input => _coop = input}/>
+				<label htmlFor="coop">Coop:</label>
+			</div>
+			<div>
+				<input id="livestream" 
+				       type="checkbox" 
+				       defaultChecked={livestream}
+				       ref={input => _livestream = input}/>
+				<label htmlFor="livestream">Livestreamed:</label>
+			</div>
+			<button>Add</button>
+		</form>
+	)
 
-		return (
-			<form onSubmit={this.submit} className="add-day-form">
-				<label htmlFor="game">Game:</label>
-				<input id="game" 
-					   type="text" 
-					   required 
-					   defaultValue={game}
-					   ref="game"/>
-				<label htmlFor="date">Date:</label>
-				<input id="date" 
-				       type="date" 
-				       required 
-				       defaultValue={date}
-				       ref="date"/>
-				<div>
-					<input id="coop" 
-						   type="checkbox" 
-						   defaultChecked={coop}
-						   ref="coop"/>
-					<label htmlFor="coop">Coop:</label>
-				</div>
-				<div>
-					<input id="livestream" 
-					       type="checkbox" 
-					       defaultChecked={livestream}
-					       ref="livestream"/>
-					<label htmlFor="livestream">Livestreamed:</label>
-				</div>
-				<button>Add</button>
-			</form>
-		)
-	}
 }
 
 AddDayForm.defaultProps = {
