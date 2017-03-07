b3670f62e1bccabc7dc8152211f07fd9a62cac6c
Colin Smith <cmsgoboston41@gmail.com>
2017-03-07 17:03:37 -0500

    Removed progress bar from GameDayCount into GoalProgress component

--- src/components/GameDayCount.js
+++ src/components/GameDayCount.js
@@ -1,45 +1,31 @@
+import { PropTypes } from 'react'
 import '../stylesheets/ui.scss'
 import FaGroup from 'react-icons/lib/fa/group'
 import FaGamepad from 'react-icons/lib/fa/gamepad'
 import FaTwitch from 'react-icons/lib/fa/twitch'
-import { PropTypes } from 'react'
 
-const percentDecimal = (decimal) => {
-	return ((decimal * 100) + '%')
-}
 
-const calcProgress = (total, goal) => {
-	return percentDecimal(total/goal)
-}
-
-export const GameDayCount = ({total=70, coop=20, livestream=10, goal=100}) => (
+export const GameDayCount = ({total=0, coop=0, livestream=0}) =>
 	<div className="game-day-count">
 		<div className="total-days">
 			<span>{total}</span>
-				<FaGamepad />
-			<span> days</span>
+			<FaGamepad />
+			<span>days</span>
 		</div>
 		<div className="coop-days">
 			<span>{coop}</span>
-				<FaGroup />
-			<span> days</span>
+			<FaGroup />
+			<span>days</span>
 		</div>
 		<div className="livestream-days">
 			<span>{livestream}</span>
-				<FaTwitch />
-			<span> days</span>
-		</div>
-		<div>
-			<span>
-				{calcProgress(total, goal)}
-			</span>
+			<FaTwitch />
+			<span>days</span>
 		</div>
 	</div>
-)
 
 GameDayCount.propTypes = {
   total: PropTypes.number,
   coop: PropTypes.number,
   livestream: PropTypes.number,
-  goal: PropTypes.number
 }
---
+++ src/containers/GameDayCount.js
@@ -0,0 +1,4 @@
+import GameDayCount from '../components/GameDayCount'
+
+export default () =>
+	<GameDayCount total={100} coop={25} livestream={10} />
