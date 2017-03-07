53b1965e526da407a3fd0f969e21c2958810b8f1
Colin Smith <cmsgoboston41@gmail.com>
2017-03-07 17:16:58 -0500

    Refactor GameDaylist

--- src/components/GameDayList.js
+++ src/components/GameDayList.js
@@ -1,58 +1,63 @@
+import { PropTypes } from 'react'
+import { Link } from 'react-router'
+import { GameDayRow } from './GameDayRow'
 import FaGroup from 'react-icons/lib/fa/group'
 import FaGamepad from 'react-icons/lib/fa/gamepad'
 import FaTwitch from 'react-icons/lib/fa/twitch'
-import { GameDayRow } from './GameDayRow'
-import { PropTypes } from 'react'
-import { Link } from 'react-router'
 
-export const GameDayList = ({days, filter}) => {
-  const filteredDays = (!filter || !filter.match(/coop|livestream/)) ?
-  	days:
-  	days.filter(day => day[filter])
+export const GameDayList = ({ days, filter, onRemoveDay=f=>f }) => {
+	const filteredDays = (!filter || !filter.match(/coop|livestream/)) ?
+	  	days :
+	  	days.filter(day => day[filter])
 
-  return (
-  	<div className="game-day-list">
-	<table>
-		<thead>
-			<tr>
-				<th>Date</th>
-				<th>Game</th>
-				<th>Coop</th>
-				<th>Livestream</th>
-			</tr>
-			<tr>
-				<td colSpan={4}>
-					<Link to="list">
-						All Days 
-					</Link>
-					<Link to="/list/coop">
-						Coop Days
-					</Link>
-					<Link to="/list/livestream">
-						Livestreaming Days
-					</Link>
-				</td>
-			</tr>
-		</thead>
-		<tbody>
-			{filteredDays.map((day, i) =>
-				<GameDayRow key={i} 
-							{...day}/>
-				)}
-		</tbody>
-	</table>
-	</div>
-  )
+	const activeFilterStyle = {
+	    textDecoration: 'none',
+	    color: 'Gray'
+	}
+
+	return (
+	  	<div className="game-day-list">
+			<table>
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>Game</th>
+						<th>Coop</th>
+						<th>Livestream</th>
+					</tr>
+					<tr>
+						<td colSpan={4}>
+							<Link to="list" style={(!filter) ? activeFilterStyle : null}>
+								All Days 
+							</Link>
+							<Link to="/list/coop" activeStyle={activeFilterStyle}>
+								Coop Days
+							</Link>
+							<Link to="/list/livestream" activeStyle={activeFilterStyle}>
+								Livestreaming Days
+							</Link>
+						</td>
+					</tr>
+				</thead>
+				<tbody>
+					{filteredDays.map((day, i) =>
+						<GameDayRow key={i} 
+									{...day}
+									onRemoveDay={onRemoveDay} 
+						/>
+						)}
+				</tbody>
+			</table>
+		</div>
+	)
 }
 
 GameDayList.propTypes = {
-	days: function(props){
-		if(!Array.isArray(props.days)) {
-			return new Error("GameDayList should be an array")
-		} else if(!props.days.length) {
-			return new Error("GameDayList must have at least one record")
-		} else {
-			return null
-		}
-	}
+	filter: PropTypes.oneOf(['coop', 'livestream']),
+	onRemoveDay: PropTypes.func,
+	days: (props) => (!Array.isArray(props.days)) ? 
+		new Error("GameDayList should be an array") :
+		(!props.days.length) ?
+			new Error("GameDayList must have at least one record") :
+			null
 }
